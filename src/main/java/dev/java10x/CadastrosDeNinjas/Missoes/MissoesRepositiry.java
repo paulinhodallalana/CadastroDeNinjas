58b359183a7305c4f782d835260d0ed6d3da781c
Paulinhodallalana <pd.lana@bol.com.br>
2026-02-12 15:37:39 -0300

    feat: adiciona NinjaRepository e NinjaService

---
+++ src/main/java/dev/java10x/CadastrosDeNinjas/Missoes/MissoesRepositiry.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastrosDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepositiry extends JpaRepository<MissoesModel, Long> {
+}
--- src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaModel.java
@@ -2,65 +2,30 @@ package dev.java10x.CadastrosDeNinjas.Ninjas;
 
 import dev.java10x.CadastrosDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
-
-
 //transforma uma classe em uma entidade(Classe que representa algo do mundo real)
 @Entity
-@Table(name = "tb_cadastro") // tudo minusculo boa pratica e o tb no inicio
+@Table(name = "tb_cadastro")// tudo minusculo boa pratica e o tb no inicio
+//lommbok
+@NoArgsConstructor // contrutor vazio
+@AllArgsConstructor // contrutor com parametro
+@Data //cria os geters e seters
 
 public class NinjaModel {
 
-
-
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
 
+    @Column(unique = true) // obrigatrio o campo do email
     private String email;
-
-    private  int idade;
-
-    // muitos ninjas para MESMA missao
-    @ManyToOne
+    private int idade;
+    // muitos ninjas para MESMA missa@ManyToOne
     @JoinColumn(name = "missoes_id") //chave estrangeira
-    private MissoesModel  missoes;
-
-
-
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
+    private MissoesModel missoes;
 }
+
