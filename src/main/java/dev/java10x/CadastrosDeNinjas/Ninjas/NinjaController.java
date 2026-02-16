6df59ca1d7dd4d94f1b76515c80b22e50879775c
Paulinhodallalana <pd.lana@bol.com.br>
2026-02-16 15:50:33 -0300

    feat: update NinjaController routes and configure Flyway migrations
    - Adjusted request mappings
    - Updated application.properties configuration
    - Updated pom.xml dependencies
    - Added Flyway migration scripts (db/)

--- src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaController.java
@@ -3,7 +3,6 @@ package dev.java10x.CadastrosDeNinjas.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/ninjas")
 
 
 public class NinjaController {
@@ -13,5 +12,36 @@ public class NinjaController {
   public String BoasVindas() {
       return"Essa é minha primeira mensagem nessa rota";
   }
+
+
+  // CRUDE
+
+   //ADICIONAR NINJA (CREAT)
+   @PostMapping ("/criar")
+    public  String criarNinja() {
+      return "Ninja criado";
+    };
+
+
+    //MOSTRAR TODOS OS NINJAS (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosNinjas() {
+        return "Mostrar Ninjas";
+    }
+
+
+    //ALTERAR POR ID(UPDATE)
+    @PutMapping("/todosID")
+    public String alterarNinjasPorId() {
+        return "Mostrar Ninjas Por Id:";
+    }
+
+    //DELETAR NINJA(REMOVE)
+   @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Ninja Deletado Por ID";
+   };
+
+
 
 }
