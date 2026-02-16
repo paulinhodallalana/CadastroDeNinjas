927a123eca093ec3e4df2ebcef6983ee2c499032
Paulinhodallalana <pd.lana@bol.com.br>
2026-02-16 16:16:43 -0300

    feat: add new controllers for Ninja and Missions
    - Created NinjaController with CRUD endpoints
    - Created MissionsController with basic endpoints
    - Updated routes for proper REST mapping
    - Prepared project for testing endpoints

---
+++ src/main/java/dev/java10x/CadastrosDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,39 @@
+package dev.java10x.CadastrosDeNinjas.Missoes;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
+
+public class MissoesController {
+
+    //CRUDE - CRIAR, MOSTRAR, INSERIR,DELETAR
+    // Get - mandar requisoçao para mostrar as requisisoes
+    //POST - MANDAR REQUISIÇÃO PARA CRIAR AS MISSOES
+    //PUT - MANRA PARA ALTERAR AS MISSOES
+    //DELETE - REMOVER AS MISSOES
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missao Criada com suceso";
+    }
+
+    @GetMapping("/listar")
+    public String listarMissoes() {
+        return "Todas as missoes";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missao alterada com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "missao deletada com sucesso";
+    }
+
+
+
+
+}
